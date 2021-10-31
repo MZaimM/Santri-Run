8cfcbc77da2c93cc68ea5663bcf6883a8b19074f
Sheva Athalla <shevaathalla@gmail.com>
2021-10-31 17:46:45 +0700

    fix jump and movement, add double jump, gravity changes

--- Assets/Script/Move.cs
+++ Assets/Script/Move.cs
@@ -3,30 +3,72 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Move : MonoBehaviour
-{
-    public float speed, kekuatanLompat;
-    public GameObject player;
-    public Rigidbody2D lompat;
+{    
+    public Rigidbody2D body;
+
+    int jumpCount = 0;
+    public int maxJumps = 2;
+
+    private bool grounded;
     // Start is called before the first frame update
     void Start()
     {
-        lompat = GetComponent<Rigidbody2D>();
-        
+        body = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            player.transform.position = new Vector2(player.transform.position.x + speed, player.transform.position.y);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        
+        float speed = 10;
 
-        }else if (Input.GetKey(KeyCode.LeftArrow))
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+        //flip body
+        if (horizontalInput > 0.01f)
         {
-            player.transform.position = new Vector2(player.transform.position.x - speed, player.transform.position.y);
-        }else if (Input.GetKey(KeyCode.UpArrow))
+            transform.localScale = Vector3.one;
+        }
+        else if (horizontalInput < -0.01f)
         {
-            lompat.AddForce(new Vector2(0, kekuatanLompat));
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        // space key pressed
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jump();
+        }
+
+        // applying gravitychange when jumping
+        if (body.velocity.y >= 0)
+        {
+            body.gravityScale = 10;
+        }
+        else if (body.velocity.y < 0)
+        {
+            body.gravityScale = 30;
+        }
+    }
+    private void jump()
+    {
+        int jumpPower = 30;
+
+        if (jumpCount <= maxJumps)
+        {
+            body.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            jumpCount++;
+        }
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collison)
+    {
+        if (collison.gameObject.tag == "Ground")
+        {
+            jumpCount = 0;            
         }
     }
 }
