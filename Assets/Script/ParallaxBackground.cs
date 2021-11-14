09c486054795d5f40fd56762916244f97e720250
Sheva Athalla <shevaathalla@gmail.com>
2021-11-14 23:15:09 +0700

    parralax effect

--- Assets/Script/ParallaxBackground.cs
+++ Assets/Script/ParallaxBackground.cs
@@ -4,21 +4,35 @@ using UnityEngine;
 
 public class ParallaxBackground : MonoBehaviour
 {
+    [SerializeField] private float parralaxEffectMultiplier;
     private Transform cameraTransform;
     private Vector3 lastCameraPosition;
+    private float textureUnitSizeX;
     // Start is called before the first frame update
     void Start()
     {
         cameraTransform = Camera.main.transform;
         lastCameraPosition = cameraTransform.position;
+
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        Texture2D texture = sprite.texture;
+
+        textureUnitSizeX = texture.width / sprite.pixelsPerUnit;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
-        transform.position += deltaMovement;
+        float parallaxEffectMultiplier = .5f;
+        transform.position += deltaMovement * parallaxEffectMultiplier;
 
         lastCameraPosition = cameraTransform.position;
+
+        if (cameraTransform.position.x - transform.position.x >= textureUnitSizeX)
+        {
+            float offsetPositionX = (cameraTransform.position.x - transform.position.x) % textureUnitSizeX;
+            transform.position = new Vector3(cameraTransform.position.x + offsetPositionX, transform.position.y);
+        }
     }
 }
